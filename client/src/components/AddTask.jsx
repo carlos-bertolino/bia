c79ce07335c394dbc4a287167ef15137969afd51
Carlos Bertolino <crgberto@gmail.com>
2026-08-10 20:11:54 +0000

    Deploy com CodePipeline CI/CD

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add New Task Miguell VIII with IA/ALB
+        Add New Task Miguell VIII with IA/ALB and CodePipeline
       </button>
       
       <Modal
